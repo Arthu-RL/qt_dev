84a8ba572ae731c536dafc9a0a4ed01f157b399a
ArthurRL <arthur.lima@mettainnovations.com.br>
2024-11-01 19:00:45 -0300

    Updated: build_run.py, push option added

--- build_run.py
+++ build_run.py
@@ -39,29 +39,47 @@ def run(project_path: str, image: str, container_name: str) -> None:
         log.error(f"Run failed with error:\n{e.stderr}")
         exit(1)
 
-if __name__ == '__main__':
-    parser: argparse.ArgumentParser = argparse.ArgumentParser(description="Build Vulkan dev image")
+def push(image: str) -> None:
+    run_command: str = f"""
+        docker push {image}
+    """
 
-    parser.add_argument('--run', dest='run', action='store_true', help='Run container instead of building', required=False)
-    parser.add_argument('-p', '--project_path', dest='project_path', type=str, help='Path to the project', default=f"{os.getenv('HOME')}/dev")
-    parser.add_argument('-ir', '--image_repo', dest='image_repo', type=str, help='Tag of the dev image', default='arthurrl')
-    parser.add_argument('-in', '--image_name', dest='image_name', type=str, help='Name of the dev image', default='vulkan_dev')
-    parser.add_argument('-it', '--image_tag', dest='image_tag', type=str, help='Tag of the dev image', default='latest')
-    parser.add_argument('-c', '--container_name', dest='container_name', type=str, help='Name of the dev container', default='vulkan-dev')
+    log.info(f"Run command:\n{run_command}")
 
-    args: argparse.Namespace = parser.parse_args()
-
-    if not os.path.isdir(args.project_path):
-        os.makedirs(args.project_path, exist_ok=True)
-
-    if not (len(args.image_name) > 0):
-        log.error(f"image_name parameter required!")
+    try:
+        result = sp.run(run_command, shell=True, check=True, capture_output=True, text=True)
+        log.info(f"Push output:\n{result.stdout}")
+    except sp.CalledProcessError as e:
+        log.error(f"Push failed with error:\n{e.stderr}")
         exit(1)
-    
-    image: str = f"{args.image_repo}/{args.image_name}:{args.image_tag}"
-    image = image.removeprefix('/')
 
-    if args.run:
-        run(project_path=args.project_path, image=image, container_name=args.container_name)
-    else:
-        build(image=image)
+
+parser: argparse.ArgumentParser = argparse.ArgumentParser(description="Build Vulkan dev image")
+
+parser.add_argument('--run', dest='run', action='store_true', help='Run container instead of building', required=False)
+parser.add_argument('--push', dest='push', action='store_true', help='Push image to dockerhub after building', required=False)
+parser.add_argument('-p', '--project_path', dest='project_path', type=str, help='Path to the project', default=f"{os.getenv('HOME')}/dev")
+parser.add_argument('-ir', '--image_repo', dest='image_repo', type=str, help='Tag of the dev image', default='arthurrl')
+parser.add_argument('-in', '--image_name', dest='image_name', type=str, help='Name of the dev image', default='vulkan_dev')
+parser.add_argument('-it', '--image_tag', dest='image_tag', type=str, help='Tag of the dev image', default='latest')
+parser.add_argument('-c', '--container_name', dest='container_name', type=str, help='Name of the dev container', default='vulkan-dev')
+
+args: argparse.Namespace = parser.parse_args()
+
+if not os.path.isdir(args.project_path):
+    os.makedirs(args.project_path, exist_ok=True)
+
+if not (len(args.image_name) > 0):
+    log.error(f"image_name parameter required!")
+    exit(1)
+
+image: str = f"{args.image_repo}/{args.image_name}:{args.image_tag}"
+image = image.removeprefix('/')
+
+if args.run:
+    run(project_path=args.project_path, image=image, container_name=args.container_name)
+else:
+    build(image=image)
+
+if args.push:
+    push(image=image)
